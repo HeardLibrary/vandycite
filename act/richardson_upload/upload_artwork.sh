a9722a502c16c50e58eeabb8af0a25d920b997cd
Steve Baskauf <steve.baskauf@vanderbilt.edu>
2023-11-05 15:37:17 -0600

    update following test uploads

---
+++ act/richardson_upload/upload_artwork.sh
@@ -0,0 +1,3 @@
+python3 ../../../linked-data/commonsbot/commonstool.py
+python3 ../../../linked-data/commonsbot/transfer_to_vanderbot.py
+python3 ../../../linked-data/vanderbot/vanderbot.py --log error_log.txt --terse true
